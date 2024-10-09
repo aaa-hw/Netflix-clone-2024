eb3351a0982741c6de71a1064e52d0068e9bbb6b
abdurehman ahmed <abdurehmanahme@gmail.com>
2024-10-09 10:49:58 +0300

    header and footer

---
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Componetnts/Header/Header'
+import Footer from '../../Componetnts/Footer/Footer'
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+} 
+
+export default Home
