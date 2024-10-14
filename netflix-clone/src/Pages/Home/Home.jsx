515234a5294a06dbaefdf68dca7a41217b0aee15
abdurehman ahmed <abdurehmanahme@gmail.com>
2024-10-14 15:30:24 +0300

    banner

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../Componetnts/Header/Header'
 import Footer from '../../Componetnts/Footer/Footer'
+import Banner from '../../Componetnts/Banner/Banner'
 const Home = () => {
   return (
     <div>
         <Header/>
+        <Banner/>
         <Footer/>
+      
+
     </div>
   )
 } 
