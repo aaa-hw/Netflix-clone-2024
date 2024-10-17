825be35208e591997d8fb103559b6ca6eb40b8ce
abdurehman ahmed <abdurehmanahme@gmail.com>
2024-10-17 09:11:40 +0300

    Rows

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../Componetnts/Header/Header'
 import Footer from '../../Componetnts/Footer/Footer'
 import Banner from '../../Componetnts/Banner/Banner'
+import Rowlist from '../../Componetnts/Rows/RowList/Rowlist.jsx'
 const Home = () => {
   return (
     <div>
         <Header/>
         <Banner/>
+        <Rowlist/>
         <Footer/>
       
 
